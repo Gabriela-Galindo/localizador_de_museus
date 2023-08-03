2428793ca40020fbde2f97bd767120cb0a88e597
Maria Gabriela Galindo <gabi_who@live.cm>
2023-08-03 16:41:30 -0300

    Adiciona reqs 03 e 04

---
+++ src/main/java/com/betrybe/museumfinder/exception/InvalidCoordinateException.java
@@ -0,0 +1,10 @@
+package com.betrybe.museumfinder.exception;
+
+/**
+ * Classe InvalidCoordinateException.
+ */
+public class InvalidCoordinateException extends RuntimeException {
+  public InvalidCoordinateException(String message) {
+    super(message);
+  }
+}
---
+++ src/main/java/com/betrybe/museumfinder/service/MuseumService.java
@@ -0,0 +1,49 @@
+package com.betrybe.museumfinder.service;
+
+import com.betrybe.museumfinder.database.MuseumFakeDatabase;
+import com.betrybe.museumfinder.exception.InvalidCoordinateException;
+import com.betrybe.museumfinder.exception.MuseumNotFoundException;
+import com.betrybe.museumfinder.model.Coordinate;
+import com.betrybe.museumfinder.model.Museum;
+import com.betrybe.museumfinder.util.CoordinateUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Classe MuseumService.
+ */
+@Service
+public class MuseumService implements MuseumServiceInterface {
+  private final MuseumFakeDatabase museumFakeDatabase;
+
+  @Autowired
+  public MuseumService(MuseumFakeDatabase museumFakeDatabase) {
+    this.museumFakeDatabase = museumFakeDatabase;
+  }
+
+  /**
+   * Método createMuseum.
+  */
+  @Override
+  public Museum createMuseum(Museum museum) {
+    if (!CoordinateUtil.isCoordinateValid(museum.getCoordinate())) {
+      throw new InvalidCoordinateException("Invalid coordinates");
+    }
+    return museumFakeDatabase.saveMuseum(museum);
+  }
+
+  @Override
+  public Museum getClosestMuseum(Coordinate coordinate, Double maxDistance) {
+    if (!CoordinateUtil.isCoordinateValid(coordinate)) {
+      throw new InvalidCoordinateException("Invalid coordinates");
+    } else {
+      return museumFakeDatabase.getClosestMuseum(coordinate, maxDistance)
+              .orElseThrow(() -> new MuseumNotFoundException("No museum found"));
+    }
+  }
+
+  @Override
+  public Museum getMuseum(Long id) {
+    return null;
+  }
+}
