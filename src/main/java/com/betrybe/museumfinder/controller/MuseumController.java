640298f8ebe4c043f0f2c4aef405e1119c0d5b5d
Maria Gabriela Galindo <gabi_who@live.cm>
2023-08-08 10:40:33 -0300

    Adiciona req06

--- src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
+++ src/main/java/com/betrybe/museumfinder/controller/MuseumController.java
@@ -2,16 +2,20 @@ package com.betrybe.museumfinder.controller;
 
 import com.betrybe.museumfinder.dto.MuseumCreationDto;
 import com.betrybe.museumfinder.dto.MuseumDto;
+import com.betrybe.museumfinder.model.Coordinate;
 import com.betrybe.museumfinder.model.Museum;
 import com.betrybe.museumfinder.service.MuseumServiceInterface;
 import com.betrybe.museumfinder.util.ModelDtoConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 /**
  * Museum Controller.
@@ -36,4 +40,24 @@ public class MuseumController {
     Museum createdMuseum = museumService.createMuseum(museumEntity);
     return ModelDtoConverter.modelToDto(createdMuseum);
   }
+
+  /**
+   * Rota Get closest.
+   */
+  @GetMapping("/closest")
+  @ResponseStatus(HttpStatus.OK)
+  public MuseumDto getClosest(
+          @RequestParam("lat") double latitude,
+          @RequestParam("lng") double longitude,
+          @RequestParam("max_dist_km") double maxDistanceKm) {
+    Coordinate coordinate = new Coordinate(latitude, longitude);
+    Museum closestMuseum = museumService.getClosestMuseum(coordinate, maxDistanceKm);
+
+    if (closestMuseum == null) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+    }
+
+    return ModelDtoConverter.modelToDto(closestMuseum);
+  }
+
 }
